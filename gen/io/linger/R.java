5be6df0163165e1a89fd18f2ebde6453de1b4ef6
Emily Pakulski <enp2111@columbia.edu>
2014-02-06 00:15:29 -0500

    Added some UI elements. Landing screens.

--- gen/io/linger/R.java
+++ gen/io/linger/R.java
@@ -10,6 +10,13 @@ package io.linger;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background=0x7f040000;
+        public static final int myBlack=0x7f040004;
+        public static final int myBlue=0x7f040001;
+        public static final int myLightGray=0x7f040003;
+        public static final int myYellow=0x7f040002;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,8 +24,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int down=0x7f020000;
@@ -26,29 +33,36 @@ public final class R {
         public static final int up=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int image_place_holder=0x7f080001;
-        public static final int list=0x7f080000;
-        public static final int name_entry=0x7f080002;
-        public static final int number_entry=0x7f080003;
+        public static final int action_settings=0x7f090003;
+        public static final int list=0x7f090000;
+        public static final int pager=0x7f090001;
+        public static final int section_label=0x7f090002;
     }
     public static final class layout {
         public static final int activity_contact_list=0x7f030000;
-        public static final int activity_home=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int list_entry=0x7f030003;
+        public static final int activity_landing=0x7f030001;
+        public static final int fragment_first=0x7f030002;
+        public static final int fragment_login=0x7f030003;
+        public static final int fragment_second=0x7f030004;
     }
     public static final class menu {
-        public static final int home=0x7f070000;
-        public static final int login=0x7f070001;
-        public static final int main=0x7f070002;
+        public static final int home=0x7f080000;
+        public static final int landing=0x7f080001;
+        public static final int login=0x7f080002;
+        public static final int main=0x7f080003;
+        public static final int second=0x7f080004;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_home=0x7f050004;
-        public static final int title_activity_main=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_home=0x7f060004;
+        public static final int title_activity_landing=0x7f060005;
+        public static final int title_activity_main=0x7f060003;
+        public static final int title_activity_second=0x7f060009;
+        public static final int title_section1=0x7f060006;
+        public static final int title_section2=0x7f060007;
+        public static final int title_section3=0x7f060008;
     }
     public static final class style {
         /** 
@@ -72,10 +86,9 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
---
+++ src/io/linger/LoginFragment.java
@@ -0,0 +1,35 @@
+package io.linger;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * The second fragment representing a section of the app, but that simply
+ * displays dummy text.
+ */
+public class LoginFragment extends Fragment {
+	/**
+	 * The fragment argument representing the section number for this
+	 * fragment.
+	 */
+	public static final String ARG_SECTION_NUMBER = "section_number";
+
+	public LoginFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_first,
+				container, false);
+		TextView dummyTextView = (TextView) rootView
+				.findViewById(R.id.section_label);
+		dummyTextView.setText(Integer.toString(getArguments().getInt(
+				ARG_SECTION_NUMBER)));
+		return rootView;
+	}
+}
