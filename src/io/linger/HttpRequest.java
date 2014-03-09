cbc5a8173d47406ea77cb0800b40fcfe59ae33df
Emily Pakulski <enp2111@columbia.edu>
2014-03-09 11:12:27 -0400

    make sure everything's up to date before Hiki gets files

--- src/io/linger/HttpRequest.java
+++ src/io/linger/HttpRequest.java
@@ -1,12 +1,11 @@
 package io.linger;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.os.AsyncTask;
 import android.util.Log;
@@ -49,47 +48,47 @@ public class HttpRequest extends AsyncTask<String, Integer, Void>
 
 	public void postData(String jsonStr, String urlString, String postHeader)
 	{
-		// my attempt: http://stackoverflow.com/questions/13911993/sending-a-json-http-post-request-from-android
-		URLConnection urlConn;
-		DataOutputStream printout;
-//		DataInputStream input;
-		try {
-			urlConn = new URL(urlString).openConnection();
-			urlConn.setDoInput(true);
-			urlConn.setDoOutput(true);
-			urlConn.setUseCaches(false);
-			urlConn.setRequestProperty("Content-Type","application/json");   
-			urlConn.setRequestProperty("Host", "linger.io");
-				urlConn.connect();
-			
-			printout = new DataOutputStream(urlConn.getOutputStream());
-			Log.v("Testing", "Being posted: " + URLEncoder.encode(jsonStr,"UTF-8"));
-			printout.writeBytes(jsonStr);
-			printout.writeUTF(URLEncoder.encode(jsonStr,"UTF-8"));
-			printout.flush();
-			printout.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+//		// my attempt: http://stackoverflow.com/questions/13911993/sending-a-json-http-post-request-from-android
+//		URLConnection urlConn;
+//		DataOutputStream printout;
+////		DataInputStream input;
+//		try {
+//			urlConn = new URL(urlString).openConnection();
+//			urlConn.setDoInput(true);
+//			urlConn.setDoOutput(true);
+//			urlConn.setUseCaches(false);
+//			urlConn.setRequestProperty("Content-Type","application/json");   
+//			urlConn.setRequestProperty("Host", "linger.io");
+//				urlConn.connect();
+//			
+//			printout = new DataOutputStream(urlConn.getOutputStream());
+//			Log.v("Testing", "Being posted: " + URLEncoder.encode(jsonStr,"UTF-8"));
+//			printout.writeBytes(jsonStr);
+//			printout.writeUTF(URLEncoder.encode(jsonStr,"UTF-8"));
+//			printout.flush();
+//			printout.close();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		HttpPost httppost = new HttpPost(URL);
+		
+		try
+		{
+			httppost.setEntity(new StringEntity(jsonStr));
+			httppost.setHeader("Accept", "application/json");
+			httppost.setHeader("Content-type", "application/json");
+			new DefaultHttpClient().execute(httppost);
+		}
+		catch (ClientProtocolException e)
+		{
 			e.printStackTrace();
 		}
-		
-//		HttpPost httppost = new HttpPost(url);
-//		
-//		try
-//		{
-//			httppost.setEntity(new StringEntity(jsonStr));
-//			httppost.setHeader("Accept", "application/json");
-//			httppost.setHeader("Content-type", "application/json");
-//			new DefaultHttpClient().execute(httppost);
-//		}
-//		catch (ClientProtocolException e)
-//		{
-//			e.printStackTrace();
-//		}
-//		catch (IOException e)
-//		{
-//			e.printStackTrace();
-//		}	
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}	
 	}
 }
 
